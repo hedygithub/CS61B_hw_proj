447dcde8c3cda83d3ce30079540d1f003db94a72
hedy <hedy.di.he@gmail.com>
2020-07-13 23:02:40 +0800

    hedy completed hw02

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,109 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
+
 public class Percolation {
+    public int[][] grid;
+    public int openSites;
+    //auto connect all items in first line
+    public WeightedQuickUnionUF set;
+    //auto connect all items in first line, last line
+    public WeightedQuickUnionUF percolationSet;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        grid = new int[N][N];
+        openSites = 0;
+        set = new WeightedQuickUnionUF(N*N);
+        percolationSet = new WeightedQuickUnionUF(N*N);
+        //0--closed, 1--open
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = 0;
+
+                //to decrease isFull running time
+                if (i == 0 && j !=0){
+                    set.union(0, j);
+                    percolationSet.union(0, j);
+                }
+
+                //to decrease percolation running time
+                if (i == N-1 && j !=0){
+                    percolationSet.union(XYto1D(N-1,0), XYto1D(N-1,j));
+                }
+            }
+        }
+    }
+
+    private int XYto1D(int x, int y) {
+        int d = x * grid.length + y;
+        return(d);
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if(!isOpen(row, col)) {
+            grid[row][col] = 1;
+            openSites += 1;
+            if (row - 1 >= 0 && isOpen(row - 1, col)) {
+                set.union(XYto1D(row, col), XYto1D(row - 1, col));
+                percolationSet.union(XYto1D(row, col), XYto1D(row - 1, col));
+            }
+            if (row + 1 < grid.length && isOpen(row + 1, col)) {
+                set.union(XYto1D(row, col), XYto1D(row + 1, col));
+                percolationSet.union(XYto1D(row, col), XYto1D(row + 1, col));
+            }
+            if (col - 1 >= 0 && isOpen(row, col - 1)) {
+                set.union(XYto1D(row, col), XYto1D(row, col - 1));
+                percolationSet.union(XYto1D(row, col), XYto1D(row, col - 1));
+            }
+            if (col + 1 < grid.length && isOpen(row, col + 1)) {
+                set.union(XYto1D(row, col), XYto1D(row, col + 1));
+                percolationSet.union(XYto1D(row, col), XYto1D(row, col + 1));
+            }
+        }
+
+    }
+
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return (grid[row][col] == 1);
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return isOpen(row, col) && set.connected(0, XYto1D(row, col));
+    }
+
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return(openSites);
+    }
+    // does the system percolate?
+    public boolean percolates() {
+        return percolationSet.connected(0, XYto1D(grid.length - 1, 0));
+    }
+
+    // use for unit testing (not required, but keep this here for the autograder)
+    public static void main(String[] args) {
+        Percolation testGrid = new Percolation(4);
+        System.out.println(testGrid.isOpen(1, 1)); //False
+        testGrid.open(1,1);
+        System.out.println(testGrid.isOpen(1, 1)); //Ture
+        testGrid.open(2,1);
+        testGrid.open(3,1);
+        System.out.println(testGrid.isFull(3,1));  //False
+        System.out.println(testGrid.percolates());          //False
+        testGrid.open(0,0);
+        testGrid.open(1,0);
+        System.out.println(testGrid.isFull(3,1));  //True
+        System.out.println(testGrid.percolates());         //True
+        testGrid.open(3,3);
+        System.out.println(testGrid.isFull(3,3));  //False
+
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,60 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    int [] openSites;
+    int gridLength;
+    private double confidenceInterval = 1.96;
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        } else {
+            //perform T times' simulations
+            openSites = new int [T];
+            gridLength = N;
+            for(int i = 0; i < T; i++){
+                PercolationFactory factory = new PercolationFactory();
+                Percolation grid = factory.make(N);
+                while(!grid.percolates()) {
+                    int row = StdRandom.uniform(N);
+                    int col = StdRandom.uniform(N);
+                    if(!grid.isOpen(row, col)) {
+                        grid.open(row, col);
+                        openSites[i] += 1;
+                    }
+                }
+            }
+
+
+        }
+    }
+
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return(StdStats.mean(openSites)/gridLength);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        double [] thresholdDiff = new double [openSites.length];
+        for (int i = 0; i < openSites.length; i++) {
+            thresholdDiff[i] = openSites[i]/gridLength - mean();
+        }
+        return(StdStats.stddev(thresholdDiff));
+    }
+
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return(mean() - confidenceInterval * stddev() / Math.sqrt(openSites.length));
+    }
+    // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return(mean() + confidenceInterval * stddev() / Math.sqrt(openSites.length));
+
+    }
 
 }
